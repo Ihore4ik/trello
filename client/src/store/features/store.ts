c260613f4f3dd3234a791b71ec8f149a40683a98
Ihor Triapitsyn <tryapitsin1990@gmail.com>
2024-04-07 15:52:15 +0300

    removed redundant files

--- client/src/store/features/store.ts
+++ client/src/store/features/store.ts
@@ -1,23 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
-// import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 import { apiSlice } from "./apiSlice";
-// import { TaskSlice } from "./cardSlice";
-// import { ListSlice } from "./listSlice";
 
 export const store = configureStore({
   reducer: {
-    // lists: ListSlice.reducer,
-    // tasks: TaskSlice.reducer,
     [apiSlice.reducerPath]: apiSlice.reducer,
   },
   middleware: (getDefaultMiddleware) =>
   getDefaultMiddleware().concat(apiSlice.middleware),
 });
-
-// export const useAppDispatch: () => typeof store.dispatch = useDispatch;
-// export const useAppSelector: TypedUseSelectorHook<
-//   ReturnType<typeof store.getState>
-// > = useSelector;
 
 setupListeners(store.dispatch);
